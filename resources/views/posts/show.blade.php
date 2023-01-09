8d5438646e922174122ec28072f42366cb13a391
fumiya-810 <fumiyasamo@yahoo.co.jp>
2023-01-09 16:47:36 +0900

    投稿詳細画面実装

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,15 +7,15 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach($posts as $post)
-                <div class='post'>
-                    <a href="posts/{{ $post->id }}"><h2 class='title'>{{ $post->title}}</h2></a>
+        <h1 class='title'>{{ $post->title }}</h1>
+        <div class='content'>
+                <div class='content_post'>
+                    <h3>本文</h3>
                     <p class='body'>{{$post->body}}</p>
                 </div>
-            @endforeach
         </div>
-        <div class='paginate'>{{$posts->links()}}</div>
+        <div class='footer'>
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
